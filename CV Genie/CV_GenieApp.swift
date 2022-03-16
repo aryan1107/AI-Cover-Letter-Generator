a6377d62487eef1c27eae057df6535cdcd592d0f
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2022-03-16 17:58:33 -0400

    Inital Setup

---
+++ CV Genie/CV_GenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CV_GenieApp.swift
+//  CV Genie
+//
+//  Created by Aryan on 2022-03-16.
+//
+
+import SwiftUI
+
+@main
+struct CV_GenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
